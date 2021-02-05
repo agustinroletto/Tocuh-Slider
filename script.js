4b8e22c982fbff7c4f4b36341d710ad8dd2e63c6
agusroletto <a.roletto@gmail.com>
2021-02-05 13:10:03 -0300

    Js terminado

--- script.js
+++ script.js
@@ -38,15 +38,60 @@ window.oncontextmenu = function (event) {
 
 function touchStart(index) {
     return function (event) {
-        isDragging= true;
+        currentIndex = index;
+        starPos = getPositionX(event);
+        isDragging = true;
+        //animación
+        animationID = requestAnimationFrame(animation)
+        slider.classList.add("grabbing")
     }
 }
 
 function touchEnd() {
-isDragging = false
+    isDragging = false
+    cancelAnimationFrame(animationID)
+
+    const movedBy = currentTranslate - prevTranslate
+
+    if (movedBy < -100 && currentIndex < slides.length - 1)
+        currentIndex += 1
+
+    if (movedBy > -100 && currentIndex > 0)
+        currentIndex -= 1
+
+    setPositionByIndex()
+
+    slider.classList.remove("grabbing")
 }
-function touchMove() {
- if (isDragging) {
-    console.log("move")
- }   
+
+function touchMove(event) {
+    if (isDragging) {
+        const currentPosition = getPositionX(event);
+        currentTranslate = prevTranslate + currentPosition - startPos
+    }
+}
+
+
+function getPositionX(event) {
+    event.type.includes("mouse") ? event.pageX :
+        event.touches[0].clientX
+}
+
+function animation() {
+    setSliderPosition()
+    if (isDragging) requestAnimationFrame(animation)
+
+
+
+}
+
+function setSliderPosition() {
+    slider.style.transform = "translateX(${currentTranslate}px)"
+
+}
+
+function setPositionByIndex() {
+    currentTranslate = currentIndex * -window.innerWidth
+    prevTranslate = currentTranslate
+    setSliderPosition()
 }
